ceabffe2227eae4ed4b3bd2dcf15cd1428d7e941
faith catherine <43315340+faithkatherine@users.noreply.github.com>
2023-05-11 19:51:23 +0300

    updates

--- OPENSKY/src/app/app.module.ts
+++ OPENSKY/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,6 +23,8 @@ import { FlightsComponent } from './flights/flights.component';
     AppRoutingModule,
     HttpClientModule,
     NgxMatDatetimePickerModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- OPENSKY/src/app/flights/flights.component.ts
+++ OPENSKY/src/app/flights/flights.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FlightsService } from '../services/flights.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-flights',
@@ -8,12 +9,19 @@ import { FlightsService } from '../services/flights.service';
 })
 export class FlightsComponent {
 
-  constructor(private flightsService: FlightsService){}
+  constructor(private flightsService: FlightsService, private fb: FormBuilder){
+    this.createForm()
+  }
+  myForm!: FormGroup;
   airPortsData!: {ICAO:string, name:string}[]
   ngOnInit(){
     this.getAirportsData();
   }
-
+  createForm(){
+    this.myForm = this.fb.group({
+      selectedAirport:
+    })
+  }
   getAirportsData(){
     this.flightsService.getAirports().subscribe((data:any) =>{
       this.airPortsData = data.map((airport:any) => {
